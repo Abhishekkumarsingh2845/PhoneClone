d04888c404cf60b9f2324fdd54568a3b86d205c2
Abhishekkumarsingh2845 <109286249+Abhishekkumarsingh2845@users.noreply.github.com>
2024-05-24 18:04:12 +0530

    insurnace  has been completed

--- Screen/Wealth.js
+++ Screen/Wealth.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native';
+import Info from '../Src/Component/Info';
 
 const Wealth = () => {
   const [selectedItem, setSelectedItem] = useState(null);
@@ -55,7 +56,6 @@ const Wealth = () => {
       image: require('../Asset/man.png'),
     },
   ];
-
 
   return (
     <View style={{flex: 1}}>
@@ -125,47 +125,15 @@ const Wealth = () => {
           ))}
         </ScrollView>
       </View>
-      <View style={{backgroundColor: 'white', width: '100%', height: 80}}>
-        <View
-          style={{width: '100%', backgroundColor: 'grey', height: 1}}></View>
-        <View>
-          <View
-            style={{
-              justifyContent: 'flex-start',
-              alignItems: 'flex-start',
-              flexDirection:'row',
-              marginTop: 12,
-              marginLeft: 22,
-            }}>
-            <View
-              style={{
-                width: 45,
-                height: 45,
-                backgroundColor: '#B8924C',
-                borderRadius: 25,
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderWidth: 3,
-                borderColor: '#B8924C',
-              }}>
-              <Image
-                source={require('../Asset/man.png')}
-                style={{width: 40, height: 40, resizeMode: 'contain'}}></Image>
-            </View>
-            <View style={{backgroundColor:'white',height:52,width:173,marginLeft:22}}>
-              <Text style={{color:'black',fontWeight:'bold',fontSize:17}}>Abhishek Singh</Text>
-              <Text style={{color:'black'}}>+91 9450321897</Text>
-              <Text style={{color:'black'}}>singhrnq231217@hmail.com</Text>
-            </View>
-            <View style={{backgroundColor:'whitesmoke',width:43,height:43,marginTop:8,marginLeft:5,justifyContent:'center',alignItems:'center',borderRadius:25,borderColor:'grey',borderWidth:1}}>
-              <Image source ={require("../Asset/more.png")} style={{width:30,height:40,resizeMode:'contain'}}>
-
-              </Image>
-            </View>
-          </View>
-         
-        </View>
-      </View>
+    <ScrollView>
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      </ScrollView>
     </View>
   );
 };
---
+++ Src/Component/Info.js
@@ -0,0 +1,71 @@
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React from 'react'
+
+const Info = ({UserImage}) => {
+  return (
+    <View style={{backgroundColor: 'white', width: '100%', height: 80}}>
+    <View
+      style={{width: '100%', backgroundColor: 'grey', height: 1}}></View>
+    <View>
+      <View
+        style={{
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          flexDirection: 'row',
+          marginTop: 12,
+          marginLeft: 22,
+        }}>
+        <View
+          style={{
+            width: 45,
+            height: 45,
+            backgroundColor: '#B8924C',
+            borderRadius: 25,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderWidth: 3,
+            borderColor: '#B8924C',
+          }}>
+          <Image
+            source={require('../../Asset/man.png')}
+            style={{width: 40, height: 40, resizeMode: 'contain'}} />
+        </View>
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: 52,
+            width: 173,
+            marginLeft: 22,
+          }}>
+          <Text style={{color: 'black', fontWeight: 'bold', fontSize: 17}}>
+            Abhishek Singh
+          </Text>
+          <Text style={{color: 'black'}}>+91 9450321897</Text>
+          <Text style={{color: 'black'}}>singhrnq231217@hmail.com</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'whitesmoke',
+            width: 43,
+            height: 43,
+            marginTop: 8,
+            marginLeft: 5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 25,
+            borderColor: 'grey',
+            borderWidth: 1,
+          }}>
+          <Image
+            source={require('../../Asset/more.png')}
+            style={{width: 30, height: 40, resizeMode: 'contain'}}></Image>
+        </View>
+      </View>
+    </View>
+  </View>
+  )
+}
+
+export default Info
+
+const styles = StyleSheet.create({})
